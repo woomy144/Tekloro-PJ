f3a0752ecc79fa847cd17f3f89d423b2752add76
woomy144 <73113918+woomy144@users.noreply.github.com>
2021-09-15 18:08:52 +0600

    Add files via upload

---
+++ js/styles.js
@@ -0,0 +1,135 @@
+
+let b = !1;
+let l = document.getElementById("play");
+let lo = document.getElementById("worldcumshot");
+let o = document.getElementById("gradientanmin");
+let os = document.getElementById("gametext");
+let cc = document.getElementById("buttmen");
+let ca = document.getElementById("buttmen");
+//switch status
+let kj = document.getElementById("gametext1");
+let ki = document.getElementById("gametext2");
+let ko = document.getElementById("gametext3");
+let kok = document.getElementById("Texxt");
+function  switch1 () {
+kok.innerText = "Tekloro.io"
+}   
+function switch2 () {
+kok.innerText = "Arras.io 3D"
+}
+function switch3 () {
+kok.innerText = "Heroes 3"
+}
+kj.onclick = () => {
+    b = !b;
+switch1();
+  };
+  ki.onclick = () => {
+    b = !b;
+switch2();
+  };
+  ko.onclick = () => {
+    b = !b;
+switch3();
+  };
+//=========================================
+setInterval(() =>{
+l.style.color = "#aaeee4"
+},1000)
+setInterval(() =>{
+    l.style.color = "#fff27c"
+    },1100)
+    setInterval(() =>{
+        l.style.color = "rgb(255 86 86 / 52%)"
+        },1200)
+        setInterval(() =>{
+            l.style.color = "rgb(82 95 193 / 86%)"
+            },1300)
+//text arras
+let ope1 = document.getElementById("a")
+let ope2 = document.getElementById("r")
+let ope3 = document.getElementById("rr")
+let ope4 = document.getElementById("aa")
+let ope5 = document.getElementById("s")
+let ope6 = document.getElementById(".")
+let ope7 = document.getElementById("i")
+let ope8 = document.getElementById("o")
+let ope9 = document.getElementById("3")
+let ope10 = document.getElementById("d")
+let ope11 = document.getElementById("dd")
+setInterval(() =>{
+    ope1.style.color = "#b10505";
+    },3000)
+    setInterval(() =>{
+        ope2.style.color = "#b105a4";
+        },3100)
+        setInterval(() =>{
+            ope3.style.color = "#051ab1";
+            },3200)
+            setInterval(() =>{
+                ope4.style.color = "#05a1b1";
+                },3300)
+                setInterval(() =>{
+                    ope5.style.color = "#08b105";
+                    },3400)
+                    setInterval(() =>{
+                        ope6.style.color = "#9eb105";
+                        },3500)
+                        setInterval(() =>{
+                            ope7.style.color = "#b17405";
+                            },3600)
+                            setInterval(() =>{
+                                ope8.style.color = "#b10505";
+                                },3700)
+                                setInterval(() =>{
+                                    ope9.style.color = "#03a9f4";
+                                    },3800)
+                                    setInterval(() =>{
+                                        ope10.style.color = "#8892b0";
+                                        },3900)
+                                        setInterval(() =>{
+                                            ope11.style.color = "#8892b0";
+                                            },4000)
+//opacity 0
+setInterval(() =>{
+    ope1.style.color = "white";
+    },5000)
+    setInterval(() =>{
+        ope2.style.color = "white";
+        },5100)
+        setInterval(() =>{
+            ope3.style.color = "white";
+            },5200)
+            setInterval(() =>{
+                ope4.style.color = "white";
+                },5300)
+                setInterval(() =>{
+                    ope5.style.color = "white";
+                    },5400)
+                    setInterval(() =>{
+                        ope6.style.color = "white";
+                        },5500)
+                        setInterval(() =>{
+                            ope7.style.color = "white";
+                            },5600)
+                            setInterval(() =>{
+                                ope8.style.color = "white";
+                                },5700)
+                                setInterval(() =>{
+                                    ope9.style.color = "white";
+                                    },5800)
+                                    setInterval(() =>{
+                                        ope10.style.color = "white";
+                                        },5900)
+                                        setInterval(() =>{
+                                            ope11.style.color = "white";
+                                            },6000)
+//Game select
+function move () {
+    lo.style.top = b ? "190px" : "120px"
+    os.style.opacity = b ? 0 : 1
+}
+lo.onclick = () => {
+    b = !b;
+move();
+  };
